e9f580c8ae2eaee354152138ec9855614ac5980f
DioSpace <zhendong2011@live.cn>
2020-10-24 08:39:58 +0800

    添加强制走代理和绕过代理检测

--- app/src/main/java/com/my/xposedbasedemo/Arrow.java
+++ app/src/main/java/com/my/xposedbasedemo/Arrow.java
@@ -1,5 +1,8 @@
 package com.my.xposedbasedemo;
 
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedBridge;
@@ -7,57 +10,119 @@ import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 public class Arrow implements IXposedHookLoadPackage {
+
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam loadPackageParam) throws Throwable {
-        // TODO Auto-generated method stub
-        XposedBridge.log("Loaded app ==== Congratulations! hook is go! =====");
         // 不是需要 Hook 的包直接返回
-        if (!loadPackageParam.packageName.equals("com.my.xposedtargetdemo"))
+        if (!loadPackageParam.packageName.equals("com.my.judge_in_proxy"))
             return;
 
         XposedBridge.log("app包名：" + loadPackageParam.packageName);
 
-        XposedHelpers.findAndHookMethod("com.my.xposedtargetdemo.Util", // 被Hook函数所在的类(包名+类名)
-                loadPackageParam.classLoader, "ordinaryFunc", // 被Hook函数的名称ordinaryFunc
-                String.class, // 被Hook函数的第一个参数String
-                String.class, // 被Hook函数的第二个参数String
-                int.class,// 被Hook函数的第三个参数integer
+        final Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("192.168.1.10", 8888));
+
+        //OkHttp 强制走代理
+        XposedHelpers.findAndHookMethod("okhttp3.OkHttpClient$Builder", // 被Hook函数所在的类(包名+类名)
+                loadPackageParam.classLoader,
+                "proxy", // 被Hook函数的名称proxy
+                java.net.Proxy.class, // 被Hook函数的第一个参数Proxy
                 new XC_MethodHook() {
 
                     @Override
                     protected void beforeHookedMethod(MethodHookParam param)
                             throws Throwable {
                         super.beforeHookedMethod(param);
-                        // Hook函数之前执行的代码
-                        // 打印方法的参数信息
-                        XposedBridge.log("param1：" + param.args[0]);
-                        XposedBridge.log("parma2：" + param.args[1]);
-                        XposedBridge.log("parma3：" + param.args[2]);
-                        // 打印堆栈查看调用关系
-                        StackTraceElement[] mylogs = new Throwable("mylogs")
-                                .getStackTrace();
-                        for (int i = 0; i < mylogs.length; i++) {
-                            XposedBridge.log("查看堆栈：" + mylogs[i].toString());
-                        }
+                        XposedBridge.log("start ================== okhttp3.OkHttpClient ===============");
+                        XposedBridge.log("OkHttpClient$Builder  proxy : " + param.args[0]);
+                        //将传入的参数  改成自己的代理
+                        param.args[0] = proxy;
                     }
 
                     @Override
                     protected void afterHookedMethod(MethodHookParam param)
                             throws Throwable {
                         super.afterHookedMethod(param);
-                        // Hook函数之后执行的代码
-                        // 获取类
-                        Class<?> clazz = param.thisObject.getClass();
-                        XposedBridge.log("要hook的方法所在的类：" + clazz.getName());
-
-                        // 获取方法的返回值
-                        Object resultString = param.getResult();
-                        XposedBridge.log("返回值：" + resultString.toString());
-                        // 修改方法的返回值
-                        param.setResult("Target was hooked!");
                     }
                 });
 
+        /*URL url = new URL(urlPath);
+        connection = (HttpURLConnection) url.openConnection(Proxy.NO_PROXY);//发起网络请求,并且不走代理的写法*/
+        //URL 强制走代理
+        XposedHelpers.findAndHookMethod("java.net.URL",// 被Hook函数所在的类(包名+类名)
+                loadPackageParam.classLoader,
+                "openConnection",// 被Hook函数的名称 openConnection
+                java.net.Proxy.class, // 被Hook函数的第一个参数Proxy
+                new XC_MethodHook() {
+                    @Override
+                    protected void beforeHookedMethod(MethodHookParam param)
+                            throws Throwable {
+                        super.beforeHookedMethod(param);
+                        XposedBridge.log("============= java.net.URL ============");
+                        XposedBridge.log("java.net.URL proxy : " + param.args[0]);
+                        param.args[0] = proxy;
+                    }
+
+                    @Override
+                    protected void afterHookedMethod(MethodHookParam param)
+                            throws Throwable {
+                        // TODO Auto-generated method stub
+                        super.afterHookedMethod(param);
+                    }
+                });
+
+        //绕过代理检测 1
+        XposedHelpers.findAndHookMethod("java.util.Properties",// 被Hook函数所在的类(包名+类名)
+                loadPackageParam.classLoader,
+                "getProperty",// 被Hook函数的名称
+                String.class, // 被Hook函数的第一个参数
+                new XC_MethodHook() {
+                    @Override
+                    protected void beforeHookedMethod(MethodHookParam param)
+                            throws Throwable {
+                        super.beforeHookedMethod(param);
+                        XposedBridge.log("============= java.util.Properties  getProperty ============");
+                        XposedBridge.log("java.util.Properties  proxy : " + param.args[0]);
+                    }
+
+                    @Override
+                    protected void afterHookedMethod(MethodHookParam param)
+                            throws Throwable {
+                        // TODO Auto-generated method stub
+                        super.afterHookedMethod(param);
+                        // 修改方法的返回值
+                        if (param.args[0].equals("http.proxyHost") || param.args[0].equals("https.proxyHost")
+                                || param.args[0].equals("http.proxyPort") || param.args[0].equals("https.proxyPort")) {
+                            param.setResult(null);
+                        }
+                    }
+                });
+
+        //绕过代理检测 2 (检测代理两种方法都可能用,绕过代理最好 1 2两种方法都用上)
+        XposedHelpers.findAndHookMethod("java.lang.System",// 被Hook函数所在的类(包名+类名)
+                loadPackageParam.classLoader,
+                "getProperty",// 被Hook函数的名称
+                String.class, // 被Hook函数的第一个参数
+                new XC_MethodHook() {
+                    @Override
+                    protected void beforeHookedMethod(MethodHookParam param)
+                            throws Throwable {
+                        super.beforeHookedMethod(param);
+                        XposedBridge.log("============= java.lang.System  getProperty ============");
+                        XposedBridge.log("java.lang.System  proxy  :  " + param.args[0]);
+                    }
+
+                    @Override
+                    protected void afterHookedMethod(MethodHookParam param)
+                            throws Throwable {
+                        // TODO Auto-generated method stub
+                        super.afterHookedMethod(param);
+                        // 修改方法的返回值
+                        if (param.args[0].equals("http.proxyHost") || param.args[0].equals("https.proxyHost")
+                                || param.args[0].equals("http.proxyPort") || param.args[0].equals("https.proxyPort")) {
+                            param.setResult(null);
+                        }
+                    }
+                });
     }
 
 }
