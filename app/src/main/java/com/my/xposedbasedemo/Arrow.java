bf99e1e8ee015a48b0dc2b788472b9093914e252
DioSpace <zhendong2011@live.cn>
2020-11-13 16:35:14 +0800

    优化
    删除一些不是必须的东西

--- app/src/main/java/com/my/xposedbasedemo/Arrow.java
+++ app/src/main/java/com/my/xposedbasedemo/Arrow.java
@@ -9,19 +9,18 @@ import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
-
 public class Arrow implements IXposedHookLoadPackage {
 
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam loadPackageParam) throws Throwable {
         // 不是需要 Hook 的包直接返回
-        if (!loadPackageParam.packageName.equals("com.my.judge_in_proxy"))
+        if (!loadPackageParam.packageName.equals("com.my.judge_in_proxy")) //com.my.judge_in_proxy
             return;
 
         XposedBridge.log("app包名：" + loadPackageParam.packageName);
 
         //自己设置的代理地址
-        final Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("192.168.1.4", 8888));
+        final Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("192.168.1.8", 8888));
 
         //OkHttp 强制走代理
         XposedHelpers.findAndHookMethod("okhttp3.OkHttpClient$Builder", // 被Hook函数所在的类(包名+类名)
@@ -44,7 +43,6 @@ public class Arrow implements IXposedHookLoadPackage {
                     protected void afterHookedMethod(MethodHookParam param)
                             throws Throwable {
                         super.afterHookedMethod(param);
-                        XposedBridge.log("end ================== okhttp3.OkHttpClient ===============");
                     }
                 });
 
@@ -70,7 +68,6 @@ public class Arrow implements IXposedHookLoadPackage {
                             throws Throwable {
                         // TODO Auto-generated method stub
                         super.afterHookedMethod(param);
-                        XposedBridge.log("end ============= java.net.URL ============");
                     }
                 });
 
@@ -98,7 +95,6 @@ public class Arrow implements IXposedHookLoadPackage {
                                 || param.args[0].equals("http.proxyPort") || param.args[0].equals("https.proxyPort")) {
                             param.setResult(null);
                         }
-                        XposedBridge.log("end ============= java.util.Properties  getProperty ============");
                     }
                 });
         //绕过代理检测 2 (检测代理两种方法都可能用,绕过代理最好 1 2两种方法都用上)
@@ -125,60 +121,9 @@ public class Arrow implements IXposedHookLoadPackage {
                                 || param.args[0].equals("http.proxyPort") || param.args[0].equals("https.proxyPort")) {
                             param.setResult(null);
                         }
-                        XposedBridge.log("end ============= java.lang.System  getProperty ============");
                     }
                 });
 
-        //Base64 入参
-        XposedHelpers.findAndHookMethod("android.util.Base64", // 被Hook函数所在的类(包名+类名)
-                loadPackageParam.classLoader,
-                "encodeToString", // 被Hook函数的名称
-                byte[].class,//参数 1
-                new XC_MethodHook() {
-
-                    @Override
-                    protected void beforeHookedMethod(MethodHookParam param)
-                            throws Throwable {
-                        super.beforeHookedMethod(param);
-                        XposedBridge.log("start =========== Base64 encodeToString   =========");
-                    }
-
-                    @Override
-                    protected void afterHookedMethod(MethodHookParam param)
-                            throws Throwable {
-                        super.afterHookedMethod(param);
-                        byte[] param1 = (byte[]) param.args[0];
-                        String param1_str = new String(param1);
-                        XposedBridge.log("传入参数 : \n" + param1_str);
-
-                        Object resultString = param.getResult();
-                        XposedBridge.log("返回值：" + resultString.toString());
-                        XposedBridge.log("end =========== Base64 encodeToString   =========");
-                    }
-                });
-
-        //打印newCall
-        XposedHelpers.findAndHookMethod("okhttp3.OkHttpClient", // 被Hook函数所在的类(包名+类名)
-                loadPackageParam.classLoader,
-                "newCall", // 被Hook函数的名称proxy
-                okhttp3.Request.class, // 被Hook函数的第一个参数
-                new XC_MethodHook() {
-
-                    @Override
-                    protected void beforeHookedMethod(MethodHookParam param)
-                            throws Throwable {
-                        super.beforeHookedMethod(param);
-                        XposedBridge.log("start ================== newCall ===============");
-                        XposedBridge.log("request : " + param.args[0]);
-                    }
-
-                    @Override
-                    protected void afterHookedMethod(MethodHookParam param)
-                            throws Throwable {
-                        super.afterHookedMethod(param);
-                        XposedBridge.log("end ================== newCall ===============");
-                    }
-                });
     }
 
 }
